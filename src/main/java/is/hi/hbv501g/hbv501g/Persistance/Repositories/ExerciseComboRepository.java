4bdb0722f1c03534884c591bc06818f1e396226d
kolbrunjulia <kjg18@hi.is>
2022-11-17 17:12:29 +0000

    JavaDoc fyrir Repositories

--- src/main/java/is/hi/hbv501g/hbv501g/Persistance/Repositories/ExerciseComboRepository.java
+++ src/main/java/is/hi/hbv501g/hbv501g/Persistance/Repositories/ExerciseComboRepository.java
@@ -6,25 +6,30 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 /******************************************************************************
- *  Nafn    : Hópur 7
- *  T-póstur: sns25@hi.is, kjg18@hi.is, hrj53@hi.is, mmo15@hi.is
+ *  Name    : Group 7
+ *  E-mail  : sns25@hi.is, kjg18@hi.is, hrj53@hi.is, mmo15@hi.is
  *
- *  Lýsing  : Repository fyrir ExerciseCombo. Inniheldur leitaraðferðir ásamt
- *  aðferðum til að vista og eyða exerciseCombo.
- *
+ *  Description  : Repository for the ExerciseCombo entity. Contains search methods
+ *  as well as methods to save and delete an exerciseCombo.
  *****************************************************************************/
 
+/**
+ * The main repository for handling exerciseCombo data.
+ */
 
 public interface ExerciseComboRepository extends JpaRepository<ExerciseCombo, Long > {
     ExerciseCombo save(ExerciseCombo exerciseCombo);
+
     void delete(ExerciseCombo exerciseCombo);
+
     List<ExerciseCombo> findAll();
-    // viljum vænt bara finna eftir w/o id
+
     ExerciseCombo findByID(long ID);
-    // TODO: 17.10.2022 athuga hvort þetta eigi að vera
-    //  listi af Excombos
+
     ExerciseCombo findByExercise(Exercise exercise);
+
     List<ExerciseCombo> findByWorkout(Workout workout);
+
     List<ExerciseCombo> findExerciseComboByWorkout(Workout workout);
 }
 
